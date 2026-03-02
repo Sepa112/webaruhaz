782c83024fe2b05bd36222e2d14e91885836d653
Szabo Istvan <pisti4395@gmail.com>
2026-03-02 17:52:56 +0100

    asdf

--- src/main.rs
+++ src/main.rs
@@ -26,20 +26,22 @@ async fn main() {
     dotenv().ok();
     let port = 4040;
     println!("running on {}", port);
-    let uri = "postgresql://postgres.xqnvldpayoaloqvqxirr:!ALMASpite456!@aws-1-eu-west-1.pooler.supabase.com:6543/postgres";
+    let uri = std::env::var("uri").unwrap();
     let pool = PgPoolOptions::new()
         .max_connections(5)
         .test_before_acquire(true)
-        .connect(uri).await.unwrap();
+        .connect(&uri).await.unwrap();
 
     //path
-    let home = warp::path::end().map(|| warp::reply::html(fs::read_to_string("html/index.html").unwrap()));
+    let home = warp::path::end().map(||{
+        let returning:String = fs::read_to_string("html/index.html").unwrap();
+        let termekkartya = fs::read_to_string("html/card.html").unwrap();
+        
+        let _ = returning.replace("TERMEKEK", "");
+        warp::reply::html(returning)
+    });
     let style = warp::path!("style.css").and(warp::fs::file("html/style.css"));
     let script = warp::path!("script.js").and(warp::fs::file("script.js"));
-
-    //"postgresql://postgres:[YOUR-PASSWORD]@db.your_project_id.supabase.co:5432/postgres";
-    //postgresql://postgres.xqnvldpayoaloqvqxirr:[YOUR-PASSWORD]@aws-1-eu-west-1.pooler.supabase.com:6543/postgres
-   // let pool = Pool::connect("postgresql://postgres.xqnvldpayoaloqvqxirr:!ALMASpite456!@aws-1-eu-west-1.pooler.supabase.com:6543/postgres").await.unwrap();
 
 
     //let num = sqlx::query("SELECT * FROM teszt").persistent(false).fetch_all(&pool).await.unwrap();
